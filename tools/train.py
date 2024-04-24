6169fd2b6d2f188ab44a42a92bafc48726f6c648
yinyipeng <71998593+YINYIPENG-EN@users.noreply.github.com>
2024-04-24 18:18:55 +0800

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import sys
 import torch
+from IPython import embed
+from torch import optim
 from torch.backends import cudnn
 sys.path.append('.')
 from config import cfg
@@ -37,6 +39,7 @@ def train(cfg, args):
             start_epoch = 0 if not args.resume else eval(args.weights.split('_')[1])
             scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
                                           cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD)
+            # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.94)
         else:
             print('Only support pretrain_choice for imagenet and self, but got {}'.format(args.pretrain_choice))
 
@@ -61,46 +64,61 @@ def train(cfg, args):
 
         arguments = {}
         if args.pretrain_choice == 'imagenet':
-            start_epoch = eval('weights/resnet50-19c8e357.pth')
+            start_epoch = 0 if not args.resume else eval(args.weights.split('_')[1])
             print('Start epoch:', start_epoch)
-            path_to_optimizer = args.weights.replace('model', 'optimizer')
-            print('Path to the checkpoint of optimizer:', path_to_optimizer)
-            path_to_center_param = args.weights.replace('model', 'center_param')
-            print('Path to the checkpoint of center_param:', path_to_center_param)
-            path_to_optimizer_center = args.weights.replace('model', 'optimizer_center')
-            print('Path to the checkpoint of optimizer_center:', path_to_optimizer_center)
+            if args.resume:
+                path_to_optimizer = args.weights.replace('model', 'optimizer')
+                print('Path to the checkpoint of optimizer:', path_to_optimizer)
+                path_to_center_param = args.weights.replace('model', 'center_param')
+                print('Path to the checkpoint of center_param:', path_to_center_param)
+                path_to_optimizer_center = args.weights.replace('model', 'optimizer_center')
+                print('Path to the checkpoint of optimizer_center:', path_to_optimizer_center)
+                # model_dict = model.state_dict()
+                # pretrained_dict = torch.load(args.weights)
+                # pretrained_dict = {k: v for k, v in pretrained_dict.items() if
+                #                    k in model_dict.keys() == pretrained_dict.keys()}
+                # model_dict.update(pretrained_dict)
+                # model.load_state_dict(model_dict)
 
-            model_dict = model.state_dict()
-            pretrained_dict = torch.load(args.weights)
-            pretrained_dict = {k: v for k, v in pretrained_dict.items() if
-                               k in model_dict.keys() == pretrained_dict.keys()}
-            model_dict.update(pretrained_dict)
-            model.load_state_dict(model_dict)
+                # optimizer_dict = optimizer.state_dict()
+                pretrained_dict_optimizer = torch.load(path_to_optimizer)
 
-            optimizer_dict = optimizer.state_dict()
-            pretrained_dict_optimizer = torch.load(path_to_optimizer)
-            pretrained_dict_optimizer = {k: v for k, v in pretrained_dict_optimizer.items() if
-                               k in optimizer_dict.keys() == pretrained_dict_optimizer.keys()}
-            optimizer_dict.update(pretrained_dict_optimizer)
-            optimizer.load_state_dict(optimizer_dict)
+                # pretrained_dict_optimizer = {k: v for k, v in pretrained_dict_optimizer.items() if
+                #                    k in optimizer_dict.keys() == pretrained_dict_optimizer.keys()}
+                # optimizer_dict.update(pretrained_dict_optimizer)
+                optimizer.load_state_dict(pretrained_dict_optimizer.state_dict())
+                for state in optimizer.state.values():
+                    for k, v in state.items():
+                        if torch.is_tensor(v):
+                            state[k] = v.cuda()
 
-            center_dict = model.state_dict()
-            pretrained_dict_center = torch.load(args.weights)
-            pretrained_dict_center = {k: v for k, v in pretrained_dict_center.items() if
-                               k in center_dict.keys() == pretrained_dict_center.keys()}
-            center_dict.update(pretrained_dict_center)
-            center_criterion.load_state_dict(center_dict)
+                # center_dict = model.state_dict()
+                # pretrained_dict_center = torch.load(args.weights)
+                # pretrained_dict_center = {k: v for k, v in pretrained_dict_center.items() if
+                #                    k in center_dict.keys() == pretrained_dict_center.keys()}
+                # center_dict.update(pretrained_dict_center)
+                # center_criterion.load_state_dict(center_dict)
+                optimizer_center_dict = torch.load(path_to_optimizer_center).state_dict()
+                optimizer_center.load_state_dict(optimizer_center_dict)
+                for state in optimizer_center.state.values():
+                    for k, v in state.items():
+                        if torch.is_tensor(v):
+                            state[k] = v.cuda()
+                center_criterion_dict = torch.load(path_to_center_param).state_dict()
+                center_criterion.load_state_dict(center_criterion_dict)
+            # scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
+            #                               cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD, start_epoch)
+        # elif args.pretrain_choice == 'imagenet':
+        #     start_epoch = 0
+        #     scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
+        #                                   cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD)
+        # scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
+        #                                   cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD) if not args.resume else WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
+        #                                   cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD, start_epoch)
 
-            optimizer_center.load_state_dict(torch.load(path_to_optimizer_center))
-            scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
-                                          cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD, start_epoch)
-        elif args.pretrain_choice == 'imagenet':
-            start_epoch = 0
-            scheduler = WarmupMultiStepLR(optimizer, cfg.SOLVER.STEPS, cfg.SOLVER.GAMMA, cfg.SOLVER.WARMUP_FACTOR,
-                                          cfg.SOLVER.WARMUP_ITERS, cfg.SOLVER.WARMUP_METHOD)
-        else:
-            print('Only support pretrain_choice for imagenet and self, but got {}'.format(args.pretrain_choice))
-
+        # else:
+        #     print('Only support pretrain_choice for imagenet and self, but got {}'.format(args.pretrain_choice))
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=2, gamma=0.94)
         do_train_with_center(
             cfg,
             model,
@@ -122,10 +140,10 @@ def train(cfg, args):
 def main():
     parser = argparse.ArgumentParser(description="Yolo v5 with ReID Baseline Training")
     parser.add_argument(
-        "--config_file", type=str, default=r"configs/softmax_triplet.yml", help="path to config file"
+        "--config_file", type=str, default=r"./configs/softmax_triplet.yml", help="path to config file"
     )
     parser.add_argument('--LAST_STRIDE', type=int, default=1, help='last stride')
-    parser.add_argument('--weights', type=str, default='weights/r50_ibn_2.pth')
+    parser.add_argument('--weights', type=str, default='./weights/r50_ibn_2.pth')
     parser.add_argument('--neck', type=str, default='bnneck', help='If train with BNNeck, options: bnneck or no')
     parser.add_argument('--test_neck', type=str, default='after', help='Which feature of BNNeck to be used for test, '
                                                                        'before or after BNNneck, options: before or '
@@ -139,6 +157,8 @@ def main():
                                                                                     "different optimizer "
                                                                                     "configuration")
     parser.add_argument('--resume', action='store_true', help='resume train')
+    parser.add_argument('--freeze', action='store_true', default=False, help='freeze train')
+    parser.add_argument('--freeze_epoch', type=int, default=20, help='freeze train epochs')
     parser.add_argument("opts", help="Modify config options using the command-line", default=None,
                         nargs=argparse.REMAINDER)
 
